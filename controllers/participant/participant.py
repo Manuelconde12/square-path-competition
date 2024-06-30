60dab08e3a7f2d199ea3726e47a5b36404e82841
Manuelconde12 <161709978+Manuelconde12@users.noreply.github.com>
2024-06-30 19:54:54 +0100

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -1,28 +1,53 @@
-"""Sample Webots controller for the square path benchmark."""
+from controller import Robot, Motor
 
-from controller import Robot
-
-# Get pointer to the robot.
+# Initialize the robot and get devices
 robot = Robot()
+leftWheel = robot.getDevice('left wheel motor')
+rightWheel = robot.getDevice('right wheel motor')
+rightWheelSensor = robot.getDevice('right wheel sensor')
+rightWheelSensor.enable(16)
 
-# Get pointer to each wheel of our robot.
-leftWheel = robot.getDevice('left wheel')
-rightWheel = robot.getDevice('right wheel')
+# Constants
+diametro_roda = 0.195
+lado_quadrado = 2
+tempo_de_passo = 16
 
-# Repeat the following 4 times (once for each side).
-for i in range(0, 4):
-    # First set both wheels to go forward, so the robot goes straight.
-    leftWheel.setPosition(1000)
-    rightWheel.setPosition(1000)
-    # Wait for the robot to reach a corner.
-    robot.step(3900)
-    # Then, set the right wheel backward, so the robot will turn right.
-    leftWheel.setPosition(1000)
-    rightWheel.setPosition(-1000)
-    # Wait until the robot has turned 90 degrees clockwise.
-    robot.step(480)
+# Cálculo do ângulo a ser percorrido pela roda na linha reta
+angulo_linha_reta = (lado_quadrado / (diametro_roda * 3.141592)) * 2 * 3.141592
+# Cálculo do ângulo a ser percorrido pela roda na viragem
+percurso_roda_viragem = (1/4) * 3.141592 * (0.33 / 2)
+angulo_viragem = (percurso_roda_viragem / (diametro_roda * 3.141592)) * 2 * 3.141592
 
-# Stop the robot when path is completed, as the robot performance
-# is only computed when the robot has stopped.
+# Configuração inicial das rodas
+leftWheel.setPosition(float('inf'))
+rightWheel.setPosition(float('inf'))
+velocidade = 2.0
+leftWheel.setVelocity(velocidade)
+rightWheel.setVelocity(velocidade)
+
+robot.step(tempo_de_passo)
+
+# Repeat the following 4 times (once for each side)
+for i in range(4):
+    valor_inicial_roda_direita = rightWheelSensor.getValue()
+    
+    # Movimentação em linha reta
+    while rightWheelSensor.getValue() - valor_inicial_roda_direita < angulo_linha_reta:
+        robot.step(tempo_de_passo)
+    
+    # Preparar para a viragem à direita
+    leftWheel.setVelocity(velocidade)
+    rightWheel.setVelocity(-velocidade)
+    valor_inicial_roda_direita = rightWheelSensor.getValue()
+    
+    # Viragem em malha fechada
+    while abs(rightWheelSensor.getValue() - valor_inicial_roda_direita) < angulo_viragem:
+        robot.step(tempo_de_passo)
+    
+    # Resetar as rodas para a próxima movimentação em linha reta
+    leftWheel.setVelocity(velocidade)
+    rightWheel.setVelocity(velocidade)
+
+# Parar o robô quando o caminho estiver completo
 leftWheel.setVelocity(0)
 rightWheel.setVelocity(0)
